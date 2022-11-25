21289200ee0653dfcd1ba97e945bb2682f8e36c3
Henrique <octaciano11@gmail.com>
2022-11-25 02:53:51 -0300

    Include header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListGroupItemComponent } from './components/list-group-item/list-group-item.component'; 
+import { ListGroupItemComponent } from './components/list-group-item/list-group-item.component';
+import { HeaderComponent } from './components/header/header.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListGroupItemComponent
+    ListGroupItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
