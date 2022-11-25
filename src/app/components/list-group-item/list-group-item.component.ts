ce2c32b212fff7b12a73683d41095b6b0161d13a
Henrique <octaciano11@gmail.com>
2022-11-25 16:13:14 -0300

    Refactor list-group-item component

--- src/app/components/list-group-item/list-group-item.component.ts
+++ src/app/components/list-group-item/list-group-item.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface Notification {
+  title: string,
+  subtitle: string,
+  date: string
+
+}
+
 @Component({
   selector: 'ntap-list-group-item',
   templateUrl: './list-group-item.component.html',
   styleUrls: ['./list-group-item.component.scss']
 })
 export class ListGroupItemComponent {
+
+  notifications: Notification[] = [
+    {
+      title: 'WhatsApp',
+      subtitle: 'Fulano mandou mensagem!',
+      date: '3 days ago'
+    },
+    {
+      title: 'Instagram',
+      subtitle: 'Ciclano começou a te seguir!',
+      date: '3 days ago'
+    },
+    {
+      title: 'Instagram',
+      subtitle: 'Beltrano iniciou um vídeo ao vivo.',
+      date: '3 days ago'
+    },
+  ]
 
 }
