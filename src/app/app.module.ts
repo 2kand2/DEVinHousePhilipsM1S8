c587ac3d8239765c51ceb1371ecfa28242cc66aa
Henrique <octaciano11@gmail.com>
2022-11-25 02:42:01 -0300

    Include list-group-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListGroupItemComponent } from './components/list-group-item/list-group-item.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListGroupItemComponent
   ],
   imports: [
     BrowserModule
